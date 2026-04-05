b36be4a53d45500abeefab2b1da7691ff780a999
Cassandra Lee <zhicheng1224@gmail.com>
2026-04-05 12:34:24 +1000

    installed tailwind dependencies

--- src/About.tsx
+++ src/About.tsx
@@ -3,7 +3,11 @@ export function About() {
     <main>
       <h1>Welcome to the about page!</h1>
       <p>
-        Here you can read all about us. We are a company that does things. We have a mission and values and a team of people who work hard to achieve our goals. We are passionate about what we do and we are always looking for new ways to innovate and improve. Thank you for visiting our about page!
+        Here you can read all about us. We are a company that does things. We
+        have a mission and values and a team of people who work hard to achieve
+        our goals. We are passionate about what we do and we are always looking
+        for new ways to innovate and improve. Thank you for visiting our about
+        page!
       </p>
     </main>
   );
