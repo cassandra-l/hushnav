d29ad542fd943098efb7343dc3fd283a7d8813c4
Cassandra Lee <zhicheng1224@gmail.com>
2026-04-07 13:27:35 +1000

    created mic button

--- src/map.tsx
+++ src/map.tsx
@@ -1,8 +1,11 @@
+import { MicButton } from "./components/mic-button";
+
 export function Map() {
   return (
     <main>
       <h1>Map Page</h1>
       <p>This is where users see the map and enter their destination.</p>
+      <MicButton />
     </main>
   );
 }
