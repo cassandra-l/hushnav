c8719eb77716f1e1fc3c52ad1802490633567a00
Sam <srat0029@student.monash.edu>
2026-04-12 11:24:50 +1000

    removed to do list

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Home } from "./home.tsx";
 import { Map } from "./map.tsx";
 import { SupportPage } from "./support-page.tsx";
-import ToDoList from "./ToDoList.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
