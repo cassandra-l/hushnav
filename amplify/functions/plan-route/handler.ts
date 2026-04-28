4fa986c8d2f7ed3a9795c78e3adf285a001ada3c
Cassandra <cmak0008@student.monash.edu>
2026-04-28 20:39:10 +1000

    fixed the handlers

--- amplify/functions/plan-route/handler.ts
+++ amplify/functions/plan-route/handler.ts
@@ -1,17 +1,28 @@
-import type { APIGatewayProxyHandler } from "aws-lambda";
 import { handlePlanRoute } from "../../navigation/handler";
 
-export const handler: APIGatewayProxyHandler = async (event) => {
+export const handler = async (event: any) => {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+      body: '',
+    }
+  }
+
   try {
     const body = event.body ? JSON.parse(event.body) : {};
-    const result = await handlePlanRoute(body);
+    const result = await handlePlanRoute(body); 
 
     return {
       statusCode: result.statusCode,
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Content-Type": "application/json",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
       },
       body: result.body,
     };
@@ -22,8 +33,8 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       statusCode: 500,
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Content-Type": "application/json",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
       },
       body: JSON.stringify({
         error: "Failed to plan route.",
