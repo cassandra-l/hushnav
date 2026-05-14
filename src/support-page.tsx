e476cc059b536bcc0024bc01557f8bd1a6c9c16f
Sam <srat0029@student.monash.edu>
2026-05-14 14:25:41 +1000

    removed issue

--- src/support-page.tsx
+++ src/support-page.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 // import { useNavigate } from "react-router-dom";
 import type { BadgeDefinition } from "./achievement-badges";
 import { BadgeUnlockedPopup } from "./components/badge-unlocked-popup";
-import SelfDiscoveryPage from "./components/self-discovery/self-discovery-page";
 import { motion } from "framer-motion";
 import {
   peekNextPendingBadgePopup,
