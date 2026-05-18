9a329e36b745b0e4e9ca811123856e15e4f41e4b
Cassandra Lee <zhicheng1224@gmail.com>
2026-05-18 12:13:37 +1000

    fixed issue

--- src/sensory-profile/sensory-result.tsx
+++ src/sensory-profile/sensory-result.tsx
@@ -94,7 +94,7 @@ export default function SensitivityResults({
 
             <button
               type="button"
-              onClick={() => navigate("/filter-page")}
+              onClick={() => navigate("/filter_page")}
               className="w-full py-4 bg-[#5A9A8E] hover:bg-[#5A9A8E]/90 text-white flex items-center justify-center gap-2 font-bold text-xs md:text-sm rounded-2xl transition-all cursor-pointer shadow-md"
             >
               <SlidersVertical size={15} />
